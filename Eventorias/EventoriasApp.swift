c277add383559bf1823a60538aed62a7ac36c91a
William-Gabriel <willx_gabriel@yahoo.com>
2025-12-15 10:24:25 +0100

    Initial Commit

---
+++ Eventorias/EventoriasApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventoriasApp.swift
+//  Eventorias
+//
+//  Created by Perez William on 15/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct EventoriasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EventoriasTests/EventoriasTests.swift
@@ -0,0 +1,17 @@
+//
+//  EventoriasTests.swift
+//  EventoriasTests
+//
+//  Created by Perez William on 15/12/2025.
+//
+
+import Testing
+@testable import Eventorias
+
+struct EventoriasTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
